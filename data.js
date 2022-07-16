564578bc49a742667588f7b2b7a9bd1a1e40b30a
WE4EVERKOMBAN <108579654+WE4EVERKOMBAN@users.noreply.github.com>
2022-07-16 12:06:19 +0530

    Update and rename data(2).js to data.js

---
+++ data.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+mongoLink:'mongodb+srv://Data1:Data1@cluster0.ekxpn.mongodb.net/?retryWrites=true&w=majority',// Database Link Of MongoDB
+
+apikey:'ajama',// Coinbase API Key
+
+secretkey:'wjaka',// Coinbase 2nd Key
+
+bot_token:'5466873914:AAFPbX5MCCw63hYOyAcgYT10uQhKwImdGjQ',// Bot Token
+
+bot_name:'Impulse_XAirdropBot',// Bot Username Without @
+
+bot_admin:895687774,// Admin Telegram Id
+
+channelsList : ['@AirdopAssasin'],// Channels List
+
+channelscheck : ['@AirdopAssasin'],// Check Channels List
+
+reffer_bonus:100000,// Refferal Bonus Amount
+
+min_wd:300000,// Min Withdrawal Amount
+
+daily_bonus:100,// Daily Bonus Amount
+
+currency:'IMX', // Bot Currency
+
+payment_channel:'@ImpulseXAirdrop'// Payment Channel Username
+
+}
